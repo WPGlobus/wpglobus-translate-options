4ee4a4c7cee51b11db9a7772ec631f9548cd866e
Alex Gor <alex.wpstarter@gmail.com>
2016-03-15 21:14:33 +0700

    added js

---
+++ includes/js/wpglobus-translate-options-customizer.js
@@ -0,0 +1,40 @@
+/**
+ * WPGlobus Translate Options Customizer
+ * Interface JS functions
+ *
+ * @since 1.4.0
+ *
+ * @package WPGlobus Translate Options
+ * @subpackage Administration
+ */
+/*jslint browser: true*/
+/*global jQuery, console, WPGlobusCore, WPGlobusCoreData, WPGlobusCustomizeOptions, WPGlobusTOCustomizer*/
+jQuery(document).ready(function($) {
+	"use strict";
+
+	if ( 'undefined' === typeof WPGlobusCustomizeOptions ) {
+		return;	
+	}
+
+	if ( 'undefined' === typeof WPGlobusTOCustomizer ) {
+		return;
+	}	
+
+	$( document ).on( 
+		'click',
+		'#accordion-panel-wpglobus_settings_panel',
+		function(ev) {
+			$( '#accordion-section-' + WPGlobusCustomizeOptions.sections.wpglobus_to_section ).css( 'display', 'block' );
+		}
+	);	
+	
+	/** open Translate Options page in new tab */
+	$( '#accordion-section-' + WPGlobusCustomizeOptions.sections.wpglobus_to_section + ' .accordion-section-title' ).off( 'click keydown' );
+	$( 'body' ).on( 
+		'click',
+		'#accordion-section-' + WPGlobusCustomizeOptions.sections.wpglobus_to_section + ' .accordion-section-title',
+		function(ev) {
+			window.open( WPGlobusTOCustomizer.toOptionPage, '_blank' );
+		}
+	);
+});
