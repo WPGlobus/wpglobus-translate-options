89766eec0ca011a84504c71e3f9ea76bb1b9481a
Alex Gor <alex.wpstarter@gmail.com>
2016-12-24 01:40:28 +0700

    version bump

--- wpglobus-translate-options.php
+++ wpglobus-translate-options.php
@@ -3,7 +3,9 @@
  * Plugin Name: WPGlobus Translate Options
  * Plugin URI: https://github.com/WPGlobus/wpglobus-translate-options
  * Description: Translate options from wp_options table for <a href="https://wordpress.org/plugins/wpglobus/">WPGlobus</a>.
- * Version: 1.4.3
+ * Text Domain: wpglobus-translate-options
+ * Domain Path: /languages/ 
+ * Version: 1.4.4
  * Author: WPGlobus
  * Author URI: http://www.wpglobus.com/
  * Network: false
@@ -27,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPGLOBUS_TRANSLATE_OPTIONS_VERSION', '1.4.3' );
+define( 'WPGLOBUS_TRANSLATE_OPTIONS_VERSION', '1.4.4' );
 
 add_filter( 'wpglobus_option_sections', 'wpglobus_add_options_section' );
 /**
@@ -803,6 +805,7 @@ if ( ! class_exists( 'WPGlobus_Translate_Options' ) ) :
 		 * Enqueue admin styles.
 		 *
 		 * @since 1.0.0
+		 * @since 1.4.4		 
 		 * @return void
 		 */		
 		function on_admin_styles() {
@@ -816,7 +819,7 @@ if ( ! class_exists( 'WPGlobus_Translate_Options' ) ) :
 			
 				wp_register_style(
 					'wpglobus-translate-options',
-					plugin_dir_url( __FILE__ ) . 'wpglobus-translate-options' . self::$_SCRIPT_SUFFIX . ".css",
+					plugin_dir_url( __FILE__ ) . 'includes/css/wpglobus-translate-options' . self::$_SCRIPT_SUFFIX . ".css",
 					array(),
 					WPGLOBUS_TRANSLATE_OPTIONS_VERSION,
 					'all'
@@ -831,6 +834,7 @@ if ( ! class_exists( 'WPGlobus_Translate_Options' ) ) :
 		 * Enqueue admin scripts.
 		 *
 		 * @since 1.0.0
+		 * @since 1.4.4
 		 * @return void
 		 */			
 		function on_admin_scripts() {
@@ -842,7 +846,7 @@ if ( ! class_exists( 'WPGlobus_Translate_Options' ) ) :
 			
 				wp_register_script(
 					'wpglobus-translate-options',
-					plugin_dir_url( __FILE__ ) . 'wpglobus-translate-options' . self::$_SCRIPT_SUFFIX . ".js",
+					plugin_dir_url( __FILE__ ) . 'includes/js/wpglobus-translate-options' . self::$_SCRIPT_SUFFIX . ".js",
 					array( 'jquery' ),
 					WPGLOBUS_TRANSLATE_OPTIONS_VERSION,
 					true
