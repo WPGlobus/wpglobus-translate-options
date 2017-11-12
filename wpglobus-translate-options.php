88612804fbdb7c19f3b4795455b7cba2f0e95546
Alex Gor <alex.wpstarter@gmail.com>
2017-11-12 12:23:13 +0700

    Version bump

--- wpglobus-translate-options.php
+++ wpglobus-translate-options.php
@@ -5,7 +5,7 @@
  * Description: Translate options from 'wp_options' table for <a href="https://wordpress.org/plugins/wpglobus/">WPGlobus</a>.
  * Text Domain: wpglobus-translate-options
  * Domain Path: /languages/
- * Version: 1.5.2
+ * Version: 1.5.3
  * Author: WPGlobus
  * Author URI: https://wpglobus.com/
  * Network: false
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPGLOBUS_TRANSLATE_OPTIONS_VERSION', '1.5.2' );
+define( 'WPGLOBUS_TRANSLATE_OPTIONS_VERSION', '1.5.3' );
 
 add_filter( 'wpglobus_option_sections', 'wpglobus_add_options_section' );
 /**
